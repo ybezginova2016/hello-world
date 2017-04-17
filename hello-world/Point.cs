d2b805de04cf3ab84695bdea10c89c3935abe238
Yulia Bezginova <yulia.bezginova2016@gmail.com>
2017-04-18 00:22:05 +0300

    new term - Encapsulation

--- hello-world/Point.cs
+++ hello-world/Point.cs
@@ -12,6 +12,16 @@ namespace hello_world
         public int y;
         public char sym;
 
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition( x, y );
